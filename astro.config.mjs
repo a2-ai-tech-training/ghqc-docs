6c1f98d7272ef19af22e8e55fef302f75c9b4fa1
wes-a2ai <wes@a2-ai.com>
2024-11-11 16:32:11 -0500

    started on installation instruction

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,23 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://a2-ai.github.io/ghqc/',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'ghqc',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/a2-ai/ghqc',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
-					// example of using explicit items
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	{ label: 'Code Annotation', slug: 'guides/code-annotation' },
-					// 	{ label: 'Component Showcase', slug: 'guides/component-showcase' },
-					// ],
-					autogenerate: { directory: 'guides' },
+					 // example of using explicit items
+					 items: [
+					 	// Each item here is one entry in the navigation menu.
+					 	{ label: 'Installation Guide', slug: 'guides/code-annotation' },
+					 	{ label: 'Component Showcase', slug: 'guides/component-showcase' },
+					],
+					// autogenerate: { directory: 'guides' },
 				},
 				{
 					label: 'Reference',
