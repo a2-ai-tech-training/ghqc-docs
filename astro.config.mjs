0ec6895bd26ec31017760064aac8652aae1f9c1a
wes-a2ai <wes@a2-ai.com>
2024-11-21 16:53:47 -0500

    infrastructure-interactions light mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,7 @@ export default defineConfig({
 					label: 'Reference',
 					items: [
 						{ label: "ghqc.app Package Dependencies", slug: 'reference/ghqcapp-dependencies' },
+						{ label: "Infrastructure Interactions", slug: 'reference/infrastructure-interactions'}
 					],
 				},
 			],
