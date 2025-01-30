1fdd040af7830508513cadf55ceb357a53abc05a
Jenna Johnson <jenna@a2-ai.com>
2025-01-30 16:42:41 -0500

    add index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	output: "static",
 	site: 'https://a2-ai-tech-training.github.io',
 	base: '/ghqc-docs/',
 	integrations: [
