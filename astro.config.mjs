9b197e64976a2779eee78a844622ef44bb1d5583
Jenna Johnson <jenna@a2-ai.com>
2025-01-31 12:34:25 -0500

    fix up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-// import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
-// import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
 	site: 'https://a2-ai-tech-training.github.io',
 	base: '/ghqc-docs/',
 	integrations: [
 		starlight({
-			title: 'ghqc',
+			title: 'My Docs',
 			social: {
-				github: 'https://github.com/a2-ai/ghqc',
+				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
-				//{
-				//	label: 'Guides',
-					 // example of using explicit items
-				//	 items: [
-					 	// Each item here is one entry in the navigation menu.
-				//	 	{ label: 'Installation Guide', slug: 'guides/code-annotation' },
-				//	 	{ label: 'Component Showcase', slug: 'guides/component-showcase' },
-				//	],
-					// autogenerate: { directory: 'guides' },
-				//},
-				//{
-				//	label: 'Reference',
-				//	autogenerate: { directory: 'reference' },
-				//},
+				{
+					label: 'Guides',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
 			],
 		}),
-		//[react()]
 	],
 });
-
-
-
